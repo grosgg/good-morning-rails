6e80ce831929aac6b4f0836e881d8f634e9da79a
grosgg <marin.jeremy@gmail.com>
2013-05-02 00:01:44 +0200

    change kunidays index action format

--- app/controllers/kunidays_controller.rb
+++ app/controllers/kunidays_controller.rb
@@ -8,13 +8,23 @@ class KunidaysController < ApiController
     nextWeek = currentDate.cweek+1
     newtWeek = 1 if thisWeek == 52
 
-    # Check if entries for this week exist
+    # Get this week entries
     tWeekKunidays = Kuniday.where("week = ? AND year = ?", currentDate.cweek, currentDate.year)
 
-    # Check if entries for next week exist
+    tWeekHash = {}
+    tWeekKunidays.each do |day|
+      tWeekHash[day.weekday] = day
+    end
+
+    # Get next week entries
     nWeekKunidays = Kuniday.where("week = ? AND year = ?", currentDate.cweek+1, currentDate.year)
 
-    results = {:tWeek => tWeekKunidays, :nWeek => nWeekKunidays}
+    nWeekHash = {}
+    nWeekKunidays.each do |day|
+      nWeekHash[day.weekday] = day
+    end
+
+    results = {:tWeek => tWeekHash, :nWeek => nWeekHash}
     respond_with results
   end
 
--- app/models/kuniday.rb
+++ app/models/kuniday.rb
@@ -1,3 +1,3 @@
 class Kuniday < ActiveRecord::Base
-  attr_accessible :date, :am, :pm, :comment
+  attr_accessible :date, :weekday, :week, :year, :am, :pm, :comment
 end
