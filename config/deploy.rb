e4d17ea7a888aa80b6ba9c0f70dff0e48916e7ae
Jeremy Marin <marin.jeremy@gmail.com>
2013-07-31 00:00:43 +0200

    Fix deploy task in mina config

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,7 +67,7 @@ task :deploy => :environment do
     invoke :'rails:assets_precompile'
 
     to :launch do
-      queue 'touch tmp/restart.txt'
+      queue %[ touch "#{deploy_to}/tmp/restart.txt" ]
     end
   end
 end
